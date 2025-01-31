787478bab1b967bb4a990254f1f33f856f7de619
nrefi321 <94899570+nrefi321@users.noreply.github.com>
2025-01-31 09:54:38 +0700

    update swagger route

--- expressrestapi/utils/swagger.js
+++ expressrestapi/utils/swagger.js
@@ -16,7 +16,7 @@ const options = {
         servers: [
             {
                 // url: `http://${process.env.HOST || 'localhost'}:${process.env.PORT || 3001}`
-                url: `http://${process.env.HOST}`
+                url: `https://${process.env.HOST}`
             },
         ],
             components: {
